626286cf473f2513e38e84ee5367e67a78a73096
AstarChen <50765596+AstarChen0720@users.noreply.github.com>
2024-06-26 22:16:32 +0800

    Add files via upload

---
+++ myfirstwebsite/scripts/main.js
@@ -0,0 +1,9 @@
+const myHeading= document.querySelector("h1")
+myHeading.textContent = "Hello WOrld"
+
+let myvariable="bruh";/* this is a comment*/
+myvariable="huh"//this is a comment 
+
+document.querySelector("html").onclick = function () {
+  alert("Ouch! Stop poking me!");
+};
